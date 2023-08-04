3a44388226f107660bbfc36d6cc70217e92a41d8
Bela D <86171066+sakikout@users.noreply.github.com>
2023-08-04 15:18:43 -0300

    Update produto.h

--- produto.h
+++ produto.h
@@ -7,12 +7,22 @@
 #include <stdlib.h>
 
 #include "utils.h"
+#include "particoes.h"
 
 typedef struct Produto {
   int cod;
   char nome[50];
   char valor[50];
 } TProd;
+
+typedef struct vetor_prod {
+  TProd *en;
+  FILE *f;
+  TParticao *particao;
+  int aux_p;
+  int read;
+
+} TVetProd;
 
 void imprime_produto(TProd *prod);
 
@@ -37,5 +47,15 @@ TProd busca_sequencial_produto(int cod, FILE *arq);
 TProd busca_binaria_produto(int cod, FILE *arq, int tam);
 
 void insertion_sort_disco_produto(FILE *arq, int tam);
+
+void classificacaoSubs_prod(FILE *arq);
+
+void intercalacao_produtos(char *nome_arquivo_saida, TNomes *nome_particoes);
+
+void processa_particoes_produtos(TVetProd **v, TNomes *nome_particoes, int num_p, char *out);
+
+void intercala_particoes_produtos(TVetProd *particao, TVetProd *particao2, TNomes *nome_particoes, int index);
+
+
 
 #endif // PRODUTO_H_INCLUDED
