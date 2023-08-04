2a3a03271a344cd9e9506cbc75d08309b1ab026a
Bela D <86171066+sakikout@users.noreply.github.com>
2023-08-04 15:14:20 -0300

    Update estoque.h

--- estoque.h
+++ estoque.h
@@ -2,6 +2,7 @@
 #define ESTOQUE_H
 
 #include "encomendas.h"
+#include "particoes.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,15 @@ typedef struct Estoque {
   // TLista produtos;
 
 } TEstoque;
+
+typedef struct vetor_est {
+  TEstoque *en;
+  FILE *f;
+  TParticao *particao;
+  int aux_p;
+  int read;
+
+} TVetEstoque;
 
 void imprime_estoque(TEstoque *estoque);
 
@@ -39,5 +49,14 @@ TEstoque busca_sequencial_estoque(int cod, FILE *arq);
 TEstoque busca_binaria_estoque(int cod, FILE *arq, int tam);
 
 void insertion_sort_disco_estoque(FILE *arq, int tam);
+
+void classificacaoSubs_estq(FILE *arq);
+
+void intercalacao_estoque(char *nome_arquivo_saida, TNomes *nome_particoes);
+
+void processa_particoes_estoque(TVetEstoque **v, TNomes *nome_particoes, int num_p, char *out);
+
+void intercala_particoes_estoque(TVetEstoque *particao, TVetEstoque *particao2, TNomes *nome_particoes, int index);
+
 
 #endif
